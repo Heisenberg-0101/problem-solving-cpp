a20f7cf7c4b25ce78e3ae58ca65dcda2434a520a
Ankit Chaudhary <ac484709@gmail.com>
2026-03-10 10:55:39 +0530

    sample code of cpp
    code for cpp

---
+++ basic_problem.cpp/sizeof.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 12;
+    float b = 2.3;
+    char ch = 'a';
+    cout<<"size of int\n"<<sizeof(a)<<endl;
+    cout<<"size of float\n"<<sizeof(b)<<endl;
+    cout<<"size of char\n"<<sizeof(ch)<<endl;
+    return 0;
+}
