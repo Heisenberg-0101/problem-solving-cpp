dc3af009ddff3cd3f80fdd37824330ee776edf97
Ankit Chaudhary <ac484709@gmail.com>
2026-03-11 22:42:36 +0530

    literals in datatype
    float and int

---
+++ basic_problem.cpp/float_literals.cpp
@@ -0,0 +1,19 @@
+//floating point literals
+
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,c,d;         //here e stand for exponant 
+
+    a = 3.13159;   
+                            // 2,-1.3 are stand for power
+    b = 3.14159E2;
+
+    c = 3.13159E-1;
+
+    d =  3.14159e3;
+
+    cout<<a<<endl<<b<<endl<<c<<endl<<d;
+
+    return 0;
+}
---
+++ basic_problem.cpp/int_literals.cpp
@@ -0,0 +1,13 @@
+//integer literals
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 25;    //decimal notation
+                   
+    int b = 031;      //octal notation
+
+    int o = 0x19;       //hexadecimal notation
+    
+    cout<<a<<endl<<b<<endl<<o;
+    return 0;
+}
