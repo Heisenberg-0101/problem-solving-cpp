6a11f21a24b58a35533e98496483c65a51507067
Ankit Chaudhary <ac484709@gmail.com>
2026-03-11 19:09:31 +0530

    basics
    ak

---
+++ basic_problem.cpp/if_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"not a prime number";
+    }else{
+        cout<<"prime number";
+    }
+
+    return 0;
+    
+}
---
+++ basic_problem.cpp/sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int apple_count{5}; //narrowing declaration
+
+    int banana_count{10};    //narrowing declaration
+    
+    int total_count{apple_count+banana_count};
+
+    cout<<"the total count is\n"<<total_count <<endl;
+
+    cout<<"the apple count\n"<<apple_count<<endl;
+
+       cout<<"the banana count\n"<<banana_count <<endl;
+
+
+    return 0;
+}
