2ffa4ee48c53928f03dcebda8c2e2fa74daa013e
Ankit Chaudhary <ac484709@gmail.com>
2026-03-16 21:03:32 +0530

    hy i am ankit

---
+++ basic_problem.cpp/age_calculater.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter your birth date"<<endl;
+
+    cin>>age;
+    int new_age = 2026-age ;
+   
+    cout<<"your age is" <<endl;
+    cout<<new_age<<endl;
+    
+
+    
+
+    return 0;
+}
