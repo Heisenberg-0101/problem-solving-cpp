90b3e8975c26dc305e8ed161bc8244f46d2892c4
Ankit Chaudhary <ac484709@gmail.com>
2026-03-27 14:48:50 +0530

    Quize game in c language
    quize game

---
+++ projects/quize_game.c
@@ -0,0 +1,252 @@
+//question quize game
+#include<stdio.h>
+char answer;
+int main(){
+    printf("welcome to quize game\n");
+
+    //question 1;
+    printf("\nQ1. what is the GDP of india in 2025?\n");
+    printf("a) 3.5 trillion dollars\n");
+    printf("b) 4.1 trillion dollars\n");
+    printf("c) 5.1 trillion dollars\n");
+    printf("d) 2.9 trillion dollars\n");
+    printf("enter your answer: ");
+    scanf("%c", &answer);
+
+    if(answer == 'b' || answer == 'B'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+
+    //question 2
+    printf("\nQ2. who is the current prime minister of india?\n");
+    printf("a) MR.Narendra Modi\n");
+    printf("b) mr.yogi adityanath\n");
+    printf("c)mr. nitish kumar\n");
+    printf("d)mr. amit shah\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'a' || answer == 'A'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 3
+    printf("\nQ3. name a weapon jisne pakistan ki gand fad di thi operation sindoor me?\n");
+    printf("a) bramhmos missile\n");
+    printf("b) akash air defence system\n");
+    printf("c) k4 rocket\n");
+    printf("d) s-400 defence system\n");
+    printf(" e)all of these: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'e' || answer == 'E'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 4
+    printf("\nQ4. which country has given number 0?\n");
+    printf("a) india\n");
+    printf("b) greece\n");
+    printf("c) egypt\n");
+    printf("d) china\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'a' || answer == 'A'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+
+     //question 5
+    printf("\nQ5. demons slayer me sabka baap kaun tha?\n");
+    printf("a) zenitsu\n");
+    printf("b) inosuke\n");
+    printf("c) nezuko\n");
+    printf("d) tanjiro\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'b' || answer == 'B'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 6;
+    printf("\nQ6. kya app lete lete mahine ke lakho kaaman chahte hai?\n");
+    printf("a) haan\n");
+    printf("b) nahi\n");
+    printf("c) depond on the situation \n");
+    printf("d) scheme kya hai\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'a' || answer == 'A'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 7;
+    printf("\nQ7. what is the oil price in india now ?\n");
+    printf("a) 100 rs per liter\n");
+    printf("b) 97 rs per liter\n");
+    printf("c) 95 rs per liter\n");
+    printf("d) 105 rs per liter\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'b' || answer == 'B'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 8;
+
+    printf("\nQ8. whiuch one is your fav character in better call soul?\n");
+    printf("a) saul goodman\n");
+    printf("b) gus fring\n");
+    printf("c) mike ehrmantraut\n");
+    printf("d) lalo salamanca\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'c' || answer == 'C'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 9
+    printf("\nQ9.26 me to duniya hi khatam hai ye line kis country ke liye hai?\n");
+    printf("a) bangladesh\n");
+    printf("b) pakistan\n");
+    printf("c) china\n");
+    printf("d) america\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'b' || answer == 'B'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 10
+    printf("\nQ10. how much rupees in 1 billion?\n");
+    printf("a) 9000 crore\n");
+    printf("b) 9100 crore\n");
+    printf("c) 8900 crore\n");
+    printf("d) 100 crore\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'a' || answer == 'A'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //qustion 11
+    printf("\nQ11. which one is your fav web series ?\n");
+    printf("a) the walking dead\n");
+    printf("b) the 100 \n");
+    printf("c) the wire\n");
+    printf("d) 3 body problem\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'd' || answer == 'D'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 12
+    printf("\nQ12. which one is your fav movie ?\n");
+    printf("a) the martion\n");
+    printf("b) interstaller\n");
+    printf("c) inception\n");
+    printf("d) the dark knight\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'b' || answer == 'B'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 13
+    printf("Q13. which one is your fav anime ?\n");
+    printf("a) solo leveling\n");
+    printf("b) naruto\n");
+    printf("c) demons slayer\n");
+    printf("d) jujustu kaisen\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+    if(answer == 'c' || answer == 'C'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 14
+    printf("\nQ14. according to you which country lead the world in next 50 years ?\n");
+    printf("a) usa\n");
+    printf("b) china\n");
+    printf("c) india\n");
+    printf("d) russia\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'c' || answer == 'C'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 15
+    printf("\nQ15.which country has potential has to end the war between usa and iran ?\n");
+    printf("a) india\n");
+    printf("b) bharat\n");
+    printf("c) hindustan\n");
+    printf("d) pakistan ka baap\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'd' || answer == 'D'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //quwstion 16
+    printf("\nQ16.which country is enter the war and end the war in just 1 day ?\n");
+    printf("a) india\n");
+    printf("b) north korea\n");
+    printf("c) russia\n");
+    printf("d) china\n");
+
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'b' || answer == 'B'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    //question 17
+    printf("\nQ17.one word for all countrys in the world ?\n");
+    printf("a) india = sabke sath accha rista hai\n");
+    printf("b) china = kuch bhi kha leta hai\n");
+    printf("c) russia = sabko akele nipta deta hai\n");
+    printf("d) north korea = jo mar pit ke liye ready rahta hai vo bhi nuclear le ke \n");
+    printf("e) all of the above\n");
+    printf("enter your answer: ");
+    scanf(" %c", &answer);
+
+    if(answer == 'e' || answer == 'E'){
+        printf("congratulations! your answer is correct\n");
+    } else {
+        printf("sorry, your answer is incorrect\n");
+    }
+    
+
+    return 0;
+}
